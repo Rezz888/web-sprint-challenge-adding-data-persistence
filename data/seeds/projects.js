88316246a703d696a836f93bf79b8fefa6cb98db
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-03-07 23:15:02 -0500

    Fixed migration file - MVP

--- data/seeds/projects.js
+++ data/seeds/projects.js
@@ -2,7 +2,7 @@ exports.seed = async function(knex) {
   await knex("projects").truncate()
 
   await knex("projects").insert([
-      { project_name: "sprint2", project_description: "pass or stay in same unit"},
+      { project_name: "sprint2", project_description: "pass or stay in same unit", project_completed: true},
       { project_name: "amazon arbitrage", project_description: "passive income"},
    ])
 };
