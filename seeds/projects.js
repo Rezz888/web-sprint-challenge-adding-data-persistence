ca334622820b9d7dac8dd3eeab040fc3f4d3e882
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-03-07 22:32:44 -0500

    MVP

--- seeds/projects.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = async function(knex) {
-   await knex("projects").truncate()
-
-   await knex("projects").insert([
-       { project_name: "sprint2", project_description: "pass or stay in same unit"},
-       { project_name: "amazon arbitrage", project_description: "passive income"},
-    ])
-};
